79a27474da6c722c9a80fec2565dcaab94c8e4ba
= <=>
2022-07-17 14:35:49 -0300

    rendering pipes

--- classes.py
+++ classes.py
@@ -6,8 +6,14 @@ class FlappyBird:
     def __init__(self):
         self.running = True
         self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.started = False
+        self.score = 0
+        self.can_create_pipe = False
 
     def fly_bird(self, bird):
+        if not self.started:
+            self.started = True
+            self.can_create_pipe = True
         bird.fly()
         
     def update_screen(self, *objects):
@@ -16,3 +22,19 @@ class FlappyBird:
     
     def handle_exit(self):
         self.running = False
+    
+    def generate_pipe(self, PipeClass):
+        self.can_create_pipe = False
+        return PipeClass()
+    
+    def remove_old_pipes(self, pipes):
+        for pipe in pipes:
+            if pipe.x + pipe.width < 0:
+                pipes.pop(pipes.index(pipe))
+    
+    def update_score(self, pipes, bird):
+        for pipe in pipes:
+            if not pipe.passed and bird.x > pipe.x + pipe.width:
+                pipe.passed = True
+                self.score += 1
+                self.can_create_pipe = True
--- components/pipe.py
+++ components/pipe.py
@@ -1,5 +1,17 @@
+from random import randrange
 from .game_object import GameObject
 import constants
 
 class Pipe(GameObject):
-    pass
+    def __init__(self):
+        self.x = constants.SCREEN_WIDTH
+        self.y = randrange(-275, -50)
+        self.images = [constants.PIPE_1, constants.PIPE_2]
+        self.image = None
+        self.width = self.images[0].get_width()
+        self.passed = False
+
+    def draw(self, screen):
+        self.x -= 2
+        super().draw(screen, self.x, self.y, image = self.images[0])
+        super().draw(screen, self.x, self.y + self.images[0].get_height() + 90, image = self.images[1])
